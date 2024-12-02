d2286c3bf4a4ca7a14daaaf9d3177fc269ac5e4d
Keerti Krishna Sreenivas S <keertikrishnasreenivas@gmail.com>
2024-12-02 18:34:27 +0530

    02.11.2024.java

---
+++ Leetcode/02.11.2024.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String str[]=sentence.split(" ");
+        for(int i=0;i<str.length;i++){
+           if(str[i].startsWith(searchWord)){
+            return i+1;
+           }
+        }
+        return -1;
+    }
+}
